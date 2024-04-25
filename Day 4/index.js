5d2c2755bbc75fe3b8e533a1a9fd09f18d5bd0e0
Utkarshha421 <165257339+Utkarshha421@users.noreply.github.com>
2024-04-26 00:26:09 +0530

    updates

---
+++ Day 4/index.js
@@ -0,0 +1,40 @@
+// = --> to assign value
+// == --> to compare only datas
+// === --> to campare datas with its datatype
+
+// console.log(20 > 30);
+// console.log(20 < 30);
+// console.log(20 <= 30);
+// console.log(30 >= 20);
+// console.log(20 != 30);
+
+// var num1=20;
+// var num2=20;
+// var num3="30";
+// console.log(num1==num2);
+// console.log(num1===num2);
+// console.log(num1===num3);
+
+// var age=20;
+// console.log(age>=18);
+// if(age>=18){
+//     console.log("Allowed to get license");
+// }else{
+//     console.log("Not allowed to get license");
+// }
+
+// Q. given a number find out number is even or odd.
+// if number is even then print 'Even' and if number is odd then print odd.
+
+let num1 = 12;
+if (num1 % 2 == 0) {
+  console.log(num1, " is Even");
+} else {
+  console.log(num1, " is Odd");
+}
+let num2 = 9;
+if (num2 % 2 == 0) {
+  console.log(num2, " is Even");
+} else {
+  console.log(num2, " is Odd");
+}
